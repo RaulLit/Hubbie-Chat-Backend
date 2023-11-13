b850f363b098453e77b4f9beaf4e64eb90e67075
Aditya Raul <raul.aditya912@gmail.com>
2023-11-13 18:12:30 +0530

    Updated User model & created Chat and Message models

--- models/User.js
+++ models/User.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
   {
+    name: {
+      type: String,
+      required: true,
+      minlength: 2,
+    },
     email: {
       type: String,
       required: true,
